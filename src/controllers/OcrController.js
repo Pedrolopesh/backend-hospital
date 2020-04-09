8b0899cc9d68f3078b54d1dd5fa9fae9eb43540e
Marco <marcoasjunior90@gmail.com>
2020-04-08 22:24:42 -0300

    Rota de registro de ocr ok

---
+++ src/controllers/OcrController.js
@@ -0,0 +1,24 @@
+const connection = require('../database/connection')
+
+module.exports = {
+
+    async create(req, res) {
+
+        const {
+            name,
+            queue_id,
+            risk_id,
+            user_id
+        } = req.body
+
+        await connection('ocr').insert({
+            name,
+            queue_id,
+            risk_id,
+            user_id
+        })
+
+        return res.status(200).send('Ocr registrada')
+        
+    }
+}
--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,15 @@ const express = require('express')
 const routes = express.Router()
 const passport = require('passport')
 const UserController = require('../controllers/UserController')
+const OcrController = require('../controllers/OcrController')
 
 routes.route('/user')
     .get((req, res) => res.send('tudo ok'))
     .post(UserController.create)
+
+routes.route('/ocr')
+    .get((req, res) => res.send('tudo ok'))
+    .post(OcrController.create)
 
 routes.post('/login', passport.authenticate('local', {
     session: false
