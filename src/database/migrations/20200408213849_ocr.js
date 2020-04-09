19ed889dc6781e95de468a6dddef25a1473a8bf2
Marco <marcoasjunior90@gmail.com>
2020-04-08 22:27:24 -0300

    fix tabelas

--- src/database/migrations/20200408213849_ocr.js
+++ src/database/migrations/20200408213849_ocr.js
@@ -2,9 +2,12 @@ exports.up = function (knex) {
     return knex.schema.createTable('ocr', function (table) {
         table.increments('id').primary()
         table.string('name').notNullable()
-        table.integer('queue_id').references('id').inTable('queue')
-        table.integer('risk_id').references('id').inTable('risk')
-        table.integer('user_id').references('id').inTable('user')
+        table.integer('queue_id').notNullable()
+        table.foreign('queue_id').references('id').inTable('queue')
+        table.integer('risk_id').notNullable()
+        table.foreign('risk_id').references('id').inTable('risk')
+        table.integer('user_id').notNullable()
+        table.foreign('user_id').references('id').inTable('user')
     })
 }
 
